1259d4d17f4b2e93416ee932e3da8e8ed79a316c
kumarannamreddi <kumar.annamreddi@gmail.com>
2018-07-19 16:25:26 +0000

    java program

---
+++ src/rectangulator.java
@@ -0,0 +1,12 @@
+public class Rectangulator{
+	public static void main(String[] args){
+		int length = Integer.parseInt(args[0]);
+		int width = Integer.parseInt(args[1]);
+
+	Rectangle myRectangle = new rectangle(length, width);
+
+	String output = String.format("***\n\nLength: %d\nWidth: %d\nArea: %d\nPerimeter: %d\n\n", my Rectangle'length, myRectangle.width, myRectangle.getArea(), myRectangle.getPerimeter());
+
+	System.out.println(output);
+	}
+}
